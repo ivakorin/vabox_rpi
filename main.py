6d70320ef109c132446264b25d67662ed9d14ee9
ivakorin <werewolfe@yandex.ru>
2021-03-23 21:05:19 +0500

    some changes

--- main.py
+++ main.py
@@ -1,15 +1,55 @@
 import time
 import traceback
 import yaml
-from EmulatorGUI import GPIO as GPIO
+# from EmulatorGUI import GPIO as GPIO
 # import RPi.GPIO as GPIO
 import dateutil.parser as dp
+import schedule
 
 from sun import SunriseSunet
 
 s = SunriseSunet()
-d = s.get_sun_data()
-import datetime
-parsed_t = dp.parse(d['sunrise'])
-t_in_seconds = parsed_t.strftime('%s')
-print(t_in_seconds)
+sunrise_start = s.sunrise_start()
+
+
+def get_light_color():
+    with open('config.yaml', 'r') as stream:
+        color_list = yaml.load(stream, Loader=yaml.Loader)
+    color_list = color_list.get('light_color_list')
+    result = color_list
+    return result
+
+
+def dawn_emulator(scene: str):
+    """
+    :param scene:
+    :return:
+    """
+    if scene == 'sunrise':
+        sunrise_duration = s.sunrise_duration()
+        color_list = get_light_color()
+        period_for_each_color = sunrise_duration / len(color_list)
+        for count, value in enumerate(color_list):
+            print(value)
+            print('sleep for:', period_for_each_color)
+            time.sleep(period_for_each_color)
+        sunset_start = s.sunset_start()
+        schedule.every().day.at(sunset_start).do(dawn_emulator, 'sunset')
+        return schedule.CancelJob
+    elif scene == 'sunset':
+        sunset_duration = s.sunset_duration()
+        color_list = get_light_color()
+        period_for_each_color = sunset_duration / len(color_list)
+        for count, value in enumerate(reversed(color_list)):
+            print(count, '>', value)
+            print('sleep for:', period_for_each_color)
+            time.sleep(period_for_each_color)
+        return schedule.CancelJob
+    else:
+        raise SystemExit('Scene must be "sunrise" or "sunset"!')
+
+
+schedule.every().day.at(sunrise_start).do(dawn_emulator, 'sunrise')
+while True:
+    schedule.run_pending()
+    time.sleep(1)
--- sun.py
+++ sun.py
@@ -1,7 +1,16 @@
 import yaml
+from pysolar.solar import *
 import datetime
-import requests
-import json
+from datetime import datetime as dt
+from datetime import timedelta
+from pysolar.util2 import get_sunrise_sunset_transit
+import pytz
+import time
+
+SUN_ZENITH_SUNRISE_SUNSET = 0  # Sunrise sunset
+SUN_ZENITH_CIVIL_TWILIGHT = 1  # Civil twilight's
+SUN_ZENITH_NAUTICAL_TWILIGHT = 2  # Nautical twilight's
+SUN_ZENITH_ASTRO_TWILIGHT = 3  # Astronomical twilight's
 
 
 class SunriseSunet:
@@ -20,25 +29,85 @@ class SunriseSunet:
                 result[k] = v
         return result
 
-    def get_sun_data(self):
-        result = {}
-        today = datetime.datetime.today().strftime('%Y-%m-%d')
+    #
+    # def get_sun_data(self):
+    #     result = {}
+    #     today = datetime.datetime.today().strftime('%Y-%m-%d')
+    #     geocode = self.__get_geocode()
+    #     url = '{0}lat={1}&lng={2}&date={3}&formatted=0'.format(self.url, geocode.get('lat'), geocode.get('lng'), today)
+    #     try:
+    #         r = requests.get(url).json()
+    #         for k, v in r.items():
+    #             if k == 'results':
+    #                 for desc, data in v.items():
+    #                     try:
+    #                         '''
+    #                         Меняю двоеточие у таймзоны, так как иначе питон не даёт преобразовать в timestamp
+    #                         '''
+    #                         d = data.replace('+00:00', '+0000')
+    #                     except AttributeError:
+    #                         d = data
+    #                     result[desc] = d
+    #         return result
+    #
+    #     except ConnectionError:
+    #         return False
+    def sunrise_duration(self):
         geocode = self.__get_geocode()
-        url = '{0}lat={1}&lng={2}&date={3}&formatted=0'.format(self.url, geocode.get('lat'), geocode.get('lng'), today)
-        try:
-            r = requests.get(url).json()
-            for k, v in r.items():
-                if k == 'results':
-                    for desc, data in v.items():
-                        try:
-                            '''
-                            Меняю двоеточие у таймзоны, так как иначе питон не даёт преобразовать в timestamp 
-                            '''
-                            d = data.replace('+00:00', '+0000')
-                        except AttributeError:
-                            d = data
-                        result[desc] = d
-            return result
+        longitude = geocode['longitude']
+        latitude = geocode['latitude']
+        date = dt.now()
+        tz = pytz.timezone('UTC')
+        date = date.replace(tzinfo=tz)
+        sunrise, sunset, transit = get_sunrise_sunset_transit(latitude, longitude, date, SUN_ZENITH_ASTRO_TWILIGHT)
+        astro_sunrise_timestamp = int(time.mktime(sunrise.timetuple()))
 
-        except ConnectionError:
-            return False
+        sunrise, sunset, transit = get_sunrise_sunset_transit(latitude, longitude, date, SUN_ZENITH_CIVIL_TWILIGHT)
+        civil_sunrise_timestamp = int(time.mktime(sunrise.timetuple()))
+        sec = civil_sunrise_timestamp - astro_sunrise_timestamp
+        return sec
+
+    def sunset_duration(self):
+        geocode = self.__get_geocode()
+        longitude = geocode['longitude']
+        latitude = geocode['latitude']
+        date = dt.now()
+        tz = pytz.timezone('UTC')
+        date = date.replace(tzinfo=tz)
+        sunrise, sunset, transit = get_sunrise_sunset_transit(latitude, longitude, date, SUN_ZENITH_ASTRO_TWILIGHT)
+        astro_sunset_timestamp = int(time.mktime(sunrise.timetuple()))
+
+        sunrise, sunset, transit = get_sunrise_sunset_transit(latitude, longitude, date, SUN_ZENITH_CIVIL_TWILIGHT)
+        civil_sunset_timestamp = int(time.mktime(sunrise.timetuple()))
+        sec = civil_sunset_timestamp - astro_sunset_timestamp
+        return sec
+
+    def sunrise_start(self):
+        geocode = self.__get_geocode()
+        longitude = geocode['longitude']
+        latitude = geocode['latitude']
+        date = dt.now()
+        tz = pytz.timezone('UTC')
+        date = date.replace(tzinfo=tz)
+        sunrise, sunset, transit = get_sunrise_sunset_transit(latitude, longitude, date, SUN_ZENITH_ASTRO_TWILIGHT)
+        result = sunrise.strftime('%H:%M:%S')
+        return result
+
+    def sunset_start(self):
+        geocode = self.__get_geocode()
+        longitude = geocode['longitude']
+        latitude = geocode['latitude']
+        date = dt.now()
+        tz = pytz.timezone('UTC')
+        date = date.replace(tzinfo=tz)
+        sunrise, sunset, transit = get_sunrise_sunset_transit(latitude, longitude, date, SUN_ZENITH_CIVIL_TWILIGHT)
+        result = sunset.strftime('%H:%M:%S')
+        return result
+
+    def sun_altitude(self):
+        geocode = self.__get_geocode()
+        date = dt.now()
+        tz = pytz.timezone('UTC')
+        date = date.replace(tzinfo=tz)
+        a = get_altitude(longitude_deg=geocode['lng'], latitude_deg=geocode['lat'], when=date, elevation=18)
+        print(a)
